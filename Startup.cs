4db836a15668efe6d3fb1ebec5bff971e7959fbb
Ashish Solanki <36642379+asolank5@users.noreply.github.com>
2020-09-09 08:22:16 -0700

    Explanation of launcSettings and appSettings JSON files and configuration in it

--- Startup.cs
+++ Startup.cs
@@ -1,10 +1,12 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -12,6 +14,12 @@ namespace EmployeeManagement
 {
     public class Startup
     {
+        private IConfiguration _config;
+
+        public Startup(IConfiguration config)
+        {
+            _config = config;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -23,11 +31,14 @@ namespace EmployeeManagement
         {
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
+                app.UseDeveloperExceptionPage();        // this is a middleware 
             }
 
             app.Run(async (context) => {
-                await context.Response.WriteAsync(System.Diagnostics.Process.GetCurrentProcess().ProcessName);
+                //await context.Response
+                //.WriteAsync(System.Diagnostics.Process.GetCurrentProcess().ProcessName);
+                await context.Response
+                .WriteAsync(_config["MyKey"]);
             });
 
             app.UseRouting();
